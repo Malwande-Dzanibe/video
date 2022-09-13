f119335af6d6ab752926abde95175a61d84cf01e
Malwande-Dzanibe <malwandedza@gmail.com>
2022-09-13 12:08:48 +0200

    video website

---
+++ video.js
@@ -0,0 +1,19 @@
+
+const video = document.querySelector(".v-one")
+const loader = document.querySelector(".loader")
+
+window.addEventListener("load", function(){
+    loader.remove()
+})
+
+
+
+
+
+
+
+
+
+
+
+
